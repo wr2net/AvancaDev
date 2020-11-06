87897cc56af789b151e2cbae9da5904955802e31
Wagner <wagner@rigolisarosa.com>
2020-11-05 22:04:12 -0300

    Desafio 01

--- c/c.go
+++ c/c.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 type Coupon struct {
@@ -47,11 +50,42 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	result := Result{Status: valid}
 
+	resultServe := makeHttpCall("http://localhost:9093/%22")
+	jsonResultserve, err := json.Marshal(resultServe)
+	if err != nil {
+		log.Fatal("Failed in converting json")
+	}
+	log.Println(string(jsonResultserve))
+
+	fmt.Fprintf(w, string(jsonResultserve))
+
 	jsonResult, err := json.Marshal(result)
 	if err != nil {
-		log.Fatal("Error converting json")
+		log.Fatal("Failed in converting json")
 	}
 
 	fmt.Fprintf(w, string(jsonResult))
+}
 
+func makeHttpCall(uriService string) Result {
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 5
+	values := url.Values{}
+
+	res, err := retryClient.PostForm(uriService, values)
+	if err != nil {
+		result := Result{Status: "Service offline"}
+		return result
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Fail in processing result")
+	}
+
+	result := Result{}
+	json.Unmarshal(data, &result)
+	return result
 }
--- d/d.go
+++ d/d.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Result struct {
+	Status string
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9093", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	result := Result{Status: "valid"}
+
+	jsonResult, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("Fail in convertint json")
+	}
+
+	fmt.Fprint(w, string(jsonResult))
+}
